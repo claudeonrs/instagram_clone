cefbe33a88039688be8ba0ab21264f8d754c7dfc
Claudeon Susanto <claudeon9j6@gmail.com>
2022-07-28 21:04:39 +0800

    Revert "Add image selection widget"
    This reverts commit 8f71a85d1cb56d1b7459487d3ce8a26d45ab70e5.

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -1,14 +1,7 @@
 import 'dart:typed_data'; // for Uint8List
-<<<<<<< Updated upstream
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-=======
-
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'storage_methods.dart';
->>>>>>> Stashed changes
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -20,7 +13,6 @@ class AuthMethods {
     required String password,
     required String username, // stored in firestore database
     required String bio, // stored in firestore database
-<<<<<<< Updated upstream
     // required Uint8List file, // a fixed-length list of 8-bit unsigned integers
   }) async {
     String res = "Some error occurred";
@@ -31,40 +23,16 @@ class AuthMethods {
               bio.isNotEmpty //||
           //file != null
           ) {
-=======
-    required Uint8List // a fixed-length list of 8-bit unsigned integers,
-        file, // * for user image
-  }) async {
-    print('f');
-    String res = "Some error occurred";
-    try {
-      if (email.isNotEmpty ||
-          password.isNotEmpty ||
-          username.isNotEmpty ||
-          bio.isNotEmpty ||
-          file != null) {
->>>>>>> Stashed changes
         //register user
         UserCredential cred = await _auth.createUserWithEmailAndPassword(
             email: email, password: password);
         print(cred.user!.uid);
 
-<<<<<<< Updated upstream
-=======
-        // ! PROBLEMATIC
-        String photoUrl = await StorageMethods()
-            .uploadImageToStorage('profilePics', file, false);
-
->>>>>>> Stashed changes
         // add user to our database
         await _firestore
             .collection('users') // add to collection 'users'
             // create one if it doesn't alr exist
             .doc(cred.user!.uid) // ! exclamation mark bcuz user can be null
-<<<<<<< Updated upstream
-=======
-            // ! makes sure the doc name and uid are the same
->>>>>>> Stashed changes
             .set({
           // if the uid already exists, then it will overwrite the existing data
           'username': username,
@@ -73,16 +41,8 @@ class AuthMethods {
           'bio': bio,
           'followers': [],
           'following': [],
-<<<<<<< Updated upstream
         });
         res = "success";
-=======
-          'photoUrl': photoUrl,
-        });
-        res = "success";
-      } else {
-        res = "Please enter all the fields";
->>>>>>> Stashed changes
       }
     } catch (err) {
       res = err.toString();
