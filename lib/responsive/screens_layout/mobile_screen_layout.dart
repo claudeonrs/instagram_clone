69e23b46e0aa684d0a7fe9c2911121aee96defec
andre <tiltedaf3@gmail.com>
2022-07-30 17:02:43 +0800

    Experiment with mobile screen display of user data

--- lib/responsive/screens_layout/mobile_screen_layout.dart
+++ lib/responsive/screens_layout/mobile_screen_layout.dart
@@ -1,12 +1,41 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class MobileScreenLayout extends StatelessWidget {
+class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({Key? key}) : super(key: key);
+
+  @override
+  State<MobileScreenLayout> createState() => _MobileScreenLayoutState();
+}
+
+class _MobileScreenLayoutState extends State<MobileScreenLayout> {
+  String username = "";
+
+  @override
+  void initState() {
+    super.initState();
+    getUsername();
+  }
+
+  void getUsername() async {
+    // make reference to collection named 'users'
+    // get() here returns a document snapShot (1-time read like an actual snapShot)
+    DocumentSnapshot snap = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .get(); // cant be null since logged in
+
+    setState(() {
+      // use Map to get around nullable property and access username property since we know it cannot be null!
+      username = (snap.data() as Map<String, dynamic>)['username'];
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("This is Mobile!")),
+      body: Center(child: Text('${username}')),
     );
   }
 }
