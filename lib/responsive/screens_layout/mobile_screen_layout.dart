90ba6ddd95038ca43af4415db9045d85bfef9299
andre <tiltedaf3@gmail.com>
2022-08-01 19:11:33 +0800

    Remove username query (test) implementation from mobile screen layout code)

--- lib/responsive/screens_layout/mobile_screen_layout.dart
+++ lib/responsive/screens_layout/mobile_screen_layout.dart
@@ -1,8 +1,6 @@
 import 'package:instagram_clone/models/user.dart' as model;
 import 'package:instagram_clone/providers/user_provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:provider/provider.dart';
 
 class MobileScreenLayout extends StatefulWidget {
@@ -13,32 +11,10 @@ class MobileScreenLayout extends StatefulWidget {
 }
 
 class _MobileScreenLayoutState extends State<MobileScreenLayout> {
-  String username = "";
-
-  @override
-  void initState() {
-    super.initState();
-    getUsername();
-  }
-
-  void getUsername() async {
-    // make reference to collection named 'users'
-    // get() here returns a document snapShot (1-time read like an actual snapShot)
-    DocumentSnapshot snap = await FirebaseFirestore.instance
-        .collection('users')
-        .doc(FirebaseAuth.instance.currentUser!.uid)
-        .get(); // cant be null since logged in
-
-    setState(() {
-      // use Map to get around nullable property and access username property since we know it cannot be null!
-      username = (snap.data() as Map<String, dynamic>)['username'];
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     model.User user = Provider.of<UserProvider>(context)
-        .getUser; // avoid same class anme clash with firebase User
+        .getUser; // avoid same class name clash with firebase User
     return Scaffold(
       body: Center(child: Text(user.email)),
     );
